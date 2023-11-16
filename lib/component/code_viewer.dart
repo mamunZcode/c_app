530001be024b274ebb98705552b40312d4833d59
Md.Muntasir Mamun <132230558+mamunZcode@users.noreply.github.com>
2023-11-17 00:00:01 +0600

    update token 2

--- lib/component/code_viewer.dart
+++ lib/component/code_viewer.dart
@@ -45,7 +45,7 @@ class _CodeViewerState extends State<CodeViewer>
     if (kIsWeb) {
       Map<String, String> header = {
         "Authorization":
-      "token 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",
+        "token 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",
         "Accept": "application/vnd.github.v3.raw"
       };
       final uri = Uri.parse(
